8c243c0be0050b2f03f37d438ad8ea30a0f599e8
damercadoo <damercadoo@unal.edu.co>
2021-04-12 20:19:21 -0500

    add bulk load

--- practices/Product Management/src/labs/pm/app/Shop.java
+++ practices/Product Management/src/labs/pm/app/Shop.java
@@ -31,44 +31,45 @@ public class Shop {
         // TODO code application logic here
         ProductManager pm = new ProductManager("en-GB");
 //        pm.CreateProduct(101, "Tea", BigDecimal.valueOf(10.99), Rating.NOT_RATED);
-        pm.parseProduct("D,101,Tea,10.99,0,2019-09-19");
+//        pm.parseProduct("D,101,Tea,10.99,0,2019-09-19");
         //pm.parseProduct("D,101,Tea,10.99,0");
         //pm.PrintProductReport(42);
 
         //pm.reviewProduct(42, Rating.ONE_STAR, "nice hot cup of tea");
-        pm.parseReview("101,1,very rich");
-        pm.parseReview("101,2,delicious");
-        pm.parseReview("101,3,I would come back");
+//        pm.parseReview("101,1,very rich");
+//        pm.parseReview("101,2,delicious");
+//        pm.parseReview("101,3,I would come back");
 //        pm.reviewProduct(101, Rating.TWO_STAR, "the best I have ever tasted");
 //        pm.reviewProduct(101, Rating.THREE_STAR, "great service");
         pm.PrintProductReport(101);
+        pm.PrintProductReport(103);
 //        pm.changeLocale("fr-FR");
         pm.CreateProduct(102, "Coffe", BigDecimal.valueOf(50.99), Rating.FOUR_STAR);
 //        pm.PrintProductReport(102);
         //pm.parseReview("1024,x, greatttt");
-        pm.parseReview("102,1,the coffe is strong enough");
-        pm.parseReview("102,2,decoration is great");
-        pm.parseReview("102,3,I would invite a friend");
-        pm.parseReview("102,5,just add more coffe");
-        pm.parseReview("102,5,the service is cool");
-        pm.parseReview("102,5,not reccomended");
-        
-        pm.parseProduct("F,103,Cake,3.99,0,2019-09-19");
-        pm.parseReview("103,1,the coffe is strong enough");
-        pm.parseReview("103,2,decoration is great");
-        pm.parseReview("103,3,I would invite a friend");
-        pm.parseReview("103,5,just add more coffe");
-        pm.parseReview("103,5,the service is cool");
-        pm.parseReview("103,5,not reccomended");
-        pm.PrintProductReport(103);
-//        pm.reviewProduct(102, Rating.FOUR_STAR, "bad service");
-//        pm.reviewProduct(102, Rating.TWO_STAR, "the coffe is too light");
-//        pm.reviewProduct(102, Rating.THREE_STAR, "it is expensive");
+//        pm.parseReview("102,1,the coffe is strong enough");
+//        pm.parseReview("102,2,decoration is great");
+//        pm.parseReview("102,3,I would invite a friend");
+//        pm.parseReview("102,5,just add more coffe");
+//        pm.parseReview("102,5,the service is cool");
+//        pm.parseReview("102,5,not reccomended");
+//        
+//        pm.parseProduct("F,103,Cake,3.99,0,2019-09-19");
+//        pm.parseReview("103,1,the coffe is strong enough");
+//        pm.parseReview("103,2,decoration is great");
+//        pm.parseReview("103,3,I would invite a friend");
+//        pm.parseReview("103,5,just add more coffe");
+//        pm.parseReview("103,5,the service is cool");
+//        pm.parseReview("103,5,not reccomended");
+//        pm.PrintProductReport(103);
+        pm.reviewProduct(102, Rating.FOUR_STAR, "bad service");
+        pm.reviewProduct(102, Rating.TWO_STAR, "the coffe is too light");
+        pm.reviewProduct(102, Rating.THREE_STAR, "it is expensive");
         pm.PrintProductReport(102);
         Comparator<Product> ratingSorter = (p1, p2) -> p2.getRating().ordinal() - p1.getRating().ordinal();
         Comparator<Product> priceSorter = (p1, p2) -> p1.getPrice().compareTo(p2.getPrice());
         pm.printproducts(p -> p.getPrice().floatValue() < 7, ratingSorter);
-
+//
         pm.getDiscounts().forEach(
                 (rating, discount) -> System.out.println(rating + "\t" + discount));
 //        pm.printproducts(priceSorter);
--- practices/Product Management/src/labs/pm/data/ProductManager.java
+++ practices/Product Management/src/labs/pm/data/ProductManager.java
@@ -16,7 +16,14 @@
  */
 package labs.pm.data;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -25,7 +32,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -52,6 +58,12 @@ public class ProductManager {
     private ResourceBundle config = ResourceBundle.getBundle("labs.pm.data.config");
     private MessageFormat reviewFormat = new MessageFormat(config.getString("review.data.format"));
     private MessageFormat productFormat = new MessageFormat(config.getString("product.data.format"));
+    private Path reportsFolder
+            = Path.of(config.getString("reports.folder"));
+    private Path dataFolder
+            = Path.of(config.getString("data.folder"));
+    private Path tempFolder
+            = Path.of(config.getString("temp.folder"));
     private static Map<String, ResourceFormatter> formatters
             = Map.of("en-GB", new ResourceFormatter(Locale.UK),
                     "en-US", new ResourceFormatter(Locale.US),
@@ -90,6 +102,7 @@ public class ProductManager {
 
     public ProductManager(String languageTag) {
         changeLocale(languageTag);
+        loadAllData();
     }
 
     public void changeLocale(String languageTag) {
@@ -152,24 +165,33 @@ public class ProductManager {
             PrintProductReport(findProduct(id));
         } catch (ProductManagerException ex) {
             logger.log(Level.INFO, ex.getMessage());
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Error printing product report " + ex.getMessage(), ex);
         }
     }
 
-    public void PrintProductReport(Product product) {
+    public void PrintProductReport(Product product) throws IOException {
         List<Review> reviews = products.get(product);
         Collections.sort(reviews);
-        StringBuilder txt = new StringBuilder();
-
-        txt.append(formatter.formatProduct(product));
-        txt.append("\n");
-        if (reviews.isEmpty()) {
-            txt.append(formatter.getText("no.reviews"));
-            txt.append("\n");
-        } else {
-            txt.append(reviews.stream()
-                    .map(r -> formatter.formatReview(r) + "\n")
-                    .collect(Collectors.joining())
-            );
+//        StringBuilder txt = new StringBuilder();
+        Path productFile = reportsFolder.resolve(
+                MessageFormat.format(
+                        config.getString("report.file"), product.getId())
+        );
+        try (PrintWriter out = new PrintWriter(
+                new OutputStreamWriter(
+                        Files.newOutputStream(productFile,
+                                StandardOpenOption.CREATE),
+                        "UTF-8"))) {
+            out.append(formatter.formatProduct(product) + System.lineSeparator());
+            if (reviews.isEmpty()) {
+                out.append(formatter.getText("no.reviews") + System.lineSeparator());
+            } else {
+                out.append(reviews.stream()
+                        .map(r -> formatter.formatReview(r) + System.lineSeparator())
+                        .collect(Collectors.joining())
+                );
+            }
         }
 
 //        for (Review review : reviews) {
@@ -185,8 +207,7 @@ public class ProductManager {
 //            txt.append(formatter.getText("no.reviews"));
 //            txt.append("\n");
 //        }
-        System.out.println(txt);
-
+//        System.out.println(txt);
     }
 
     public void printproducts(Predicate<Product> filter, Comparator<Product> sorter) {
@@ -208,16 +229,67 @@ public class ProductManager {
         System.out.println(txt);
     }
 
-    public void parseReview(String text) {
+    private void loadAllData() {
         try {
-            Object[] values = reviewFormat.parse(text);
-            reviewProduct(Integer.parseInt((String) values[0]), Rateable.convert(Integer.parseInt((String) values[1])), (String) values[2]);
-        } catch (ParseException | NumberFormatException ex) {
-            logger.log(Level.WARNING, "Error parsing review" + text);
+            products = Files.list(dataFolder)
+                    .filter(file -> file.getFileName().toString().startsWith("product"))
+                    .map(file -> loadProduct(file))
+                    .filter(product -> product != null)
+                    .collect(Collectors.toMap(product -> product, product -> loadReviews(product)));
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE,  "Error loading product " + ex.getMessage());
         }
     }
 
-    public void parseProduct(String text) {
+    private Product loadProduct(Path file) {
+        Product product = null;
+
+        try {
+            product = parseProduct(Files.lines(dataFolder.resolve(file), Charset.forName("UTF-8"))
+                    .findFirst().orElseThrow());
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "Error loading product " + ex.getMessage(),ex);
+        }
+        return product;
+    }
+
+    private List<Review> loadReviews(Product product) {
+        List<Review> reviews = null;
+        Path file
+                = dataFolder.resolve(
+                        MessageFormat.format(
+                                config.getString("reviews.data.file"), product.getId()
+                        )
+                );
+        if (Files.notExists(file)) {
+            reviews = new ArrayList<>();
+        } else {
+            try {
+                reviews = Files.lines(file, Charset.forName("UTF-8"))
+                        .map(text -> parseReview(text))
+                        .filter(review -> review != null)
+                        .collect(Collectors.toList());
+            } catch (IOException ex) {
+                logger.log(Level.WARNING, "Error loading reviews " + ex.getMessage());
+            }
+        }
+        return reviews;
+    }
+
+    private Review parseReview(String text) {
+        Review review = null;
+        try {
+            Object[] values = reviewFormat.parse(text);
+            //reviewProduct(Integer.parseInt((String) values[0]), Rateable.convert(Integer.parseInt((String) values[1])), (String) values[2]);
+            review = new Review(Rateable.convert(Integer.parseInt((String) values[0])), (String) values[1]);
+        } catch (ParseException | NumberFormatException ex) {
+            logger.log(Level.WARNING, "Error parsing review" + text);
+        }
+        return review;
+    }
+
+    private Product parseProduct(String text) {
+        Product product = null;
         try {
             Object[] values = productFormat.parse(text);
             int id = Integer.parseInt((String) values[1]);
@@ -226,15 +298,18 @@ public class ProductManager {
             Rating rating = Rateable.convert(Integer.parseInt((String) values[4]));
             switch ((String) values[0]) {
                 case "D":
-                    CreateProduct(id, name, price, rating);
+                    //CreateProduct(id, name, price, rating);
+                    product = new Drink(id, name, price, rating);
                     break;
                 case "F":
                     LocalDate bestBefore = LocalDate.parse((String) values[5]);
-                    CreateProduct(id, name, price, rating,bestBefore);
+                    product = new Food(id, name, price, rating, bestBefore);
+                //CreateProduct(id, name, price, rating, bestBefore);
             }
         } catch (ParseException | NumberFormatException | DateTimeException ex) {
-            logger.log(Level.WARNING,"Error parsing product: "+ text+" "+ex.getMessage());
+            logger.log(Level.WARNING, "Error parsing product: " + text + " " + ex.getMessage());
         }
+        return product;
     }
 
     public Map<String, String> getDiscounts() {
